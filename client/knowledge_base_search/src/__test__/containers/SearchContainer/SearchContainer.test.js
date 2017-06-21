2631e7dbb8707d7a9d965013fe2bbf3157d65d0c
Zachary Dernar <Zachary.M.Dernar@ibm.com>
2017-06-21 18:22:29 -0400

    Zmdernar/wildcard questions (#15)
    * Adds wildcard canned questions to kbs
    
    currently wip: has a few bugs with the button interactions
    
    * Handles question click separately
    
    May still need to look into the networking of this implementation
    
    * Finishes up design for wildcard canned questions v1
    
    Pending on if we change the results page searchcontainer state,
    this will be done if we are not collapsing the header.
    
    * Minimal code refactor
    
    Some overlapping css stuff. Oops.
    
    * removes csstransitiongroup from searchcontainer
    
    it wasn't used anymore so a warning was appearing. oops.
    
    * Removed unnecessary <span>'s and updated tests
    
    SearchContainer's tests now match the new behavior

--- client/knowledge_base_search/src/__test__/containers/SearchContainer/SearchContainer.test.js
+++ client/knowledge_base_search/src/__test__/containers/SearchContainer/SearchContainer.test.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchContainer from '../../../containers/SearchContainer/SearchContainer';
 import { TextInput } from 'watson-react-components';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
+import { Pane } from 'watson-react-components';
 
 describe('<SearchContainer />', () => {
   const onSubmitMock = jest.fn();
@@ -17,31 +18,54 @@ describe('<SearchContainer />', () => {
       />, div);
   });
 
-  it('has a header, TextInput, and button on initial load', () => {
+  it('has the random query buttons displayed on initial load', () => {
     const wrapper = shallow(
                       <SearchContainer
                         onSubmit={onSubmitMock}
                         hasResults={false}
                         isFetching={false}  />
                     );
-    expect(wrapper.find('.header--input')).toHaveLength(1);
-    expect(wrapper.find(TextInput)).toHaveLength(1);
-    expect(wrapper.find('.white--button')).toHaveLength(1);
+    expect(wrapper.find('.clickable-tabs')).toHaveLength(1);
+    expect(wrapper.find('.clickable-tab')).toHaveLength(5);
   });
 
-  it('has does not have a header or button after results fetched', () => {
+  describe('when the Custom Query tab is pressed', () => {
+    const wrapper = mount(
+                      <SearchContainer
+                        onSubmit={onSubmitMock}
+                        hasResults={false}
+                        isFetching={false}  />
+                    );
+
+    beforeEach(() => {
+      wrapper.find('.tab-panels--tab.base--a').at(1).simulate('click');
+    });
+
+    it('has the text search, icon, and submit button displayed', () => {
+      expect(wrapper.find('.positioned--icon')).toHaveLength(1);
+      expect(wrapper.find('.text-input--input')).toHaveLength(1);
+      expect(wrapper.find('.white--button')).toHaveLength(1);
+    });
+  });
+
+  describe('when a random query button is pressed', () => {
     const wrapper = shallow(
                       <SearchContainer
                         onSubmit={onSubmitMock}
-                        hasResults={true}
+                        hasResults={false}
                         isFetching={false}  />
                     );
-    expect(wrapper.find('.header--input')).toHaveLength(0);
-    expect(wrapper.find(TextInput)).toHaveLength(1);
-    expect(wrapper.find('.white--button')).toHaveLength(0);
+
+    beforeEach(() => {
+      wrapper.find('.clickable-tab').at(0).simulate('click');
+    });
+
+    it('calls onSubmit with one of the random queries', () => {
+      expect(onSubmitMock).toBeCalledWith(expect.any(String));
+    });
   });
 
-  describe('when text is typed in', () => {
+  describe('when a query is submitted', () => {
     const wrapper = shallow(
                       <SearchContainer
                         onSubmit={onSubmitMock}
@@ -65,29 +89,6 @@ describe('<SearchContainer />', () => {
     });
   });
 
-  describe('when the random query button is pressed', () => {
-    const wrapper = shallow(
-                      <SearchContainer
-                        onSubmit={onSubmitMock}
-                        hasResults={false}
-                        isFetching={false}  />
-                    );
-
-    beforeEach(() => {
-      wrapper.find('.random-query--button').simulate('click');
-    });
-
-    describe('and the form is submitted', () => {
-      beforeEach(() => {
-        wrapper.find('form').simulate('submit', { preventDefault: () => {}});
-      });
-
-      it('calls onSubmit with one of the random queries', () => {
-        expect(onSubmitMock).toBeCalledWith(expect.any(String));
-      });
-    });
-  });
-
   describe('when isFetching is true', () => {
     let wrapper;
 
@@ -100,10 +101,23 @@ describe('<SearchContainer />', () => {
                 );
     });
 
-    it('disables all the inputs', () => {
-      expect(wrapper.find(TextInput).props().disabled).toBe(true);
-      expect(wrapper.find('.white--button').props().disabled).toBe(true);
-      expect(wrapper.find('.random-query--button').props().disabled).toBe(true);
+    describe('and the Preset Queries tab is selected', () => {
+      it('disables all the random query buttons', () => {
+        wrapper.find('.clickable-tab').nodes.forEach(function(button) {
+          expect(button.props.disabled).toBe(true);
+        });
+      });
+    });
+
+    describe('and the Custom Query tab is selected', () => {
+      beforeEach(() => {
+        wrapper.find('.tab-panels--tab.base--a').at(1).simulate('click');
+      });
+
+      it('disables all the inputs', () => {
+        expect(wrapper.find(TextInput).props().disabled).toBe(true);
+        expect(wrapper.find('.white--button').props().disabled).toBe(true);
+      });
     });
   });
 });
--- client/knowledge_base_search/src/containers/SearchContainer/SearchContainer.js
+++ client/knowledge_base_search/src/containers/SearchContainer/SearchContainer.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
-import { Icon, TextInput } from 'watson-react-components';
+import { Icon, TextInput, Tabs, Pane } from 'watson-react-components';
 import randomQueries from '../../utils/randomQueries';
 import 'watson-react-components/dist/css/watson-react-components.css';
 import './styles.css';
@@ -9,7 +8,8 @@ import './styles.css';
 class SearchContainer extends Component {
   componentWillMount() {
     this.state = {
-      search_input: ''
+      search_input: '',
+      random_queries: this.getFiveRandomQueries(),
     };
   }
 
@@ -19,12 +19,19 @@ class SearchContainer extends Component {
     }
   }
 
-  handleOnClick = () => {
-    const totalQueries = randomQueries.length;
-    const randomInt = Math.floor(Math.random() * totalQueries);
+  getFiveRandomQueries() {
+    const allQueries = randomQueries.slice(0);
+    let myQueries = [];
+    for(let i = 0; i < 5; i++) {
+      let myIndex = Math.floor(Math.random() * allQueries.length);
+      myQueries.push(allQueries.splice(myIndex, 1)[0]);
+    }
+    return myQueries;
+  }
 
-    this.handleOnInput({'target': { 'value': randomQueries[randomInt]}});
-    this.props.onSubmit(randomQueries[randomInt]);
+  handleQuestionClick(query, index) {
+    this.handleOnInput({'target': { 'value': query}});
+    this.props.onSubmit(query);
   }
 
   handleOnInput = (e) => {
@@ -43,62 +50,51 @@ class SearchContainer extends Component {
   render() {
     return (
       <section className="_full-width-row search_container--section">
-        <div className="_container">
-          <CSSTransitionGroup
-            transitionName='search_header'
-            transitionEnterTimeout={500}
-            transitionLeaveTimeout={300}
-          >
-            {!this.props.hasResults
-              ? (
-                <h2 className="header--input" key='header_input'>
-                  Use a community's expertise to amplify the way you find information.
-                </h2>
-                )
-              : null
-            }
-          </CSSTransitionGroup>
+        <div className="_container _container_large">
           <form onSubmit={this.handleOnSubmit}>
-            <div className="positioned--icon">
-              <Icon type="search" />
-            </div>
-            <TextInput
-              id="search_input"
-              placeholder="Enter words, phrase, or a question about travel"
-              value={this.state.search_input}
-              onInput={this.handleOnInput}
-              style={{width: '100%'}}
-              disabled={this.props.isFetching}
-            />
-            <div className="_container-right">
-              <button
-                type="button"
-                className="random-query--button"
-                onClick={this.handleOnClick}
-                disabled={this.props.isFetching}
-              >
-                Random Query
-              </button>
-            </div>
-            <CSSTransitionGroup
-              transitionName='search_button'
-              transitionEnterTimeout={500}
-              transitionLeaveTimeout={300}
-            >
-              {!this.props.hasResults
-                ? (
-                  <div className="_container-center">
-                    <button
-                      className="white--button"
-                      disabled={this.props.isFetching}
-                    >
-                      Retrieve Answers
-                    </button>
-                  </div>
-                  )
-                : null
-              }
-            </CSSTransitionGroup>
+            <Tabs selected={0}>
+              <Pane label="Preset questions">
+              <div className='clickable-tabs'>
+                {
+                  this.state.random_queries.map((query, index) => {
+                    return (
+                      <span key={'questionButton-'+index}>
+                        <button className={query === this.state.search_input
+                          ? 'active_button clickable-tab'
+                          : 'clickable-tab'}
+                          type='button'
+                          disabled={this.props.isFetching}
+                          onClick={() => {
+                            this.handleQuestionClick(query, index);
+                          }}>
+                            {query}
+                        </button>
+                      </span>
+                    )
+                  })
+                }
+              </div>
+              </Pane>
+              <Pane label='Custom question'>
+                <span className="positioned--icon">
+                  <Icon type="search" />
+                </span>
+                <TextInput
+                  id="search_input"
+                  placeholder="Enter words, phrase, or a question about travel"
+                  value={this.state.search_input}
+                  onInput={this.handleOnInput}
+                  style={{width: '100%'}}
+                  disabled={this.props.isFetching}
+                />
+                <button
+                  className="white--button"
+                  disabled={this.props.isFetching}
+                >
+                  Retrieve Answers
+                </button>
+              </Pane>
+            </Tabs>
           </form>
         </div>
       </section>
