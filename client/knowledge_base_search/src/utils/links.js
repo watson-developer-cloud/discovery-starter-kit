17d9da0b6c5af0e41b654825e60135b4a613069c
ZACHARY M. DERNAR <Zachary.M.Dernar@ibm.com>
2017-09-11 13:59:33 -0400

    Updates doc url to bluemix

--- client/knowledge_base_search/src/utils/links.js
+++ client/knowledge_base_search/src/utils/links.js
@@ -1,7 +1,7 @@
 const links = {
   starter_kits: 'https://www.ibm.com/watson/developercloud/starter-kits.html',
   repository: 'https://github.com/watson-developer-cloud/discovery-starter-kit',
-  doc_homepage: 'http://www.ibm.com/watson/developercloud/doc/discovery',
+  doc_homepage: 'https://console.bluemix.net/docs/services/discovery/index.html',
   doc_api: 'http://www.ibm.com/watson/developercloud/discovery/api',
   bluemix: 'https://console.ng.bluemix.net/registration/?target=/catalog/services/discovery/',
   cc_license: 'https://creativecommons.org/licenses/by-sa/3.0/',
