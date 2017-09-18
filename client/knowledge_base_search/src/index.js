1ac5f9483f34ebcd606475e4ea9017dab9ff6444
ZACHARY M. DERNAR <Zachary.M.Dernar@ibm.com>
2017-09-18 17:16:35 -0400

    Moves Array.findIndex polyfill to main dependencies to support IE11

--- client/knowledge_base_search/src/index.js
+++ client/knowledge_base_search/src/index.js
@@ -1,4 +1,6 @@
 /* eslint-disable react/jsx-filename-extension */
+import 'phantomjs-polyfill-find-index/findIndex-polyfill';
+import 'phantomjs-polyfill-find/find-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
